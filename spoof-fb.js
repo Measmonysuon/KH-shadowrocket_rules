6b74a5b2a74f3815b463e9829ff257dd1f79c35a
Measmonysuon <measmonysuon@yahoo.com>
2025-08-06 05:06:27 -0700

    Update spoof-fb.js

--- spoof-fb.js
+++ spoof-fb.js
@@ -1,19 +1,9 @@
-// fb-header-spoof.js
-// Spoof device model and iOS version only if FBLC=en_US is found in User-Agent
+// ua-rewrite.js
 
-let headers = $request.headers;
+const ua = $request.headers['User-Agent'] || "";
 
-if (headers && headers["User-Agent"] && headers["User-Agent"].includes("FBLC/en_US")) {
-    let ua = headers["User-Agent"];
-
-    // Replace FBDV (device)
-    ua = ua.replace(/FBDV\/[^;]+;/, "FBDV/iPhone15,3;");
-
-    // Replace FBSV (iOS version)
-    ua = ua.replace(/FBSV\/[^;]+;/, "FBSV/16.3.1;");
-
-    // Return modified headers
-    $done({ headers: { ...headers, "User-Agent": ua } });
-} else {
-    $done({});
+if (ua.includes("en_US") || ua.includes("us_US") || ua.includes("Qaau_US")) {
+  $request.headers['User-Agent'] = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/21F80 [FBAN/FBIOS;FBDV/iPhone15,2;FBMD/iPhone;FBSN/iOS;FBSV/17.5.1;FBSS/3;FBID/phone;FBLC/en_US;FBOP/5]";
 }
+
+$done({ headers: $request.headers });
